747cdb3595db409d821cc0e7cce3245a6980709c
Lucas Madeira <lmadeira.ec@gmail.com>
2023-04-25 11:23:49 -0400

    Finalizando classes de dominio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import br.com.lucasmadeira.dominio.*;
+
 public class Main {
     public static void main(String[] args)  {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java 11");
+        curso1.setDescricao("Curso de java 11");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Maven");
+        curso2.setDescricao("Curso de Maven");
+        curso2.setCargaHoraria(4);
+
+        Conteudo mentoria = new Mentoria();
+        mentoria.setTitulo("Abstraindo um bootcamp usando Orientacao Objetos");
+        mentoria.setDescricao("Abstraindo um bootcamp usando Orientacao Objetos");
+
+
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java developer");
+        bootcamp.setDescricao("Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Java Developer 1");
+        dev1.inscreverBootcamp(bootcamp);
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Java Developer 2");
+        dev2.inscreverBootcamp(bootcamp);
+
+        while(dev1.getConteudosInscritos().isEmpty() == false || dev2.getConteudosInscritos().isEmpty() ==false){
+            System.out.println("Conteudos Inscritos dev1:"+ dev1.getConteudosInscritos());
+            System.out.println("Conteudos Concluidos dev1:"+ dev1.getConteudosConcluidos());
+
+            System.out.println("Conteudos Inscritos dev2:"+ dev2.getConteudosInscritos());
+            System.out.println("Conteudos Concluidos dev2:"+ dev2.getConteudosConcluidos());
+
+            dev1.progredir();
+            dev2.progredir();
+
+            System.out.println("XP dev1:"+ dev1.calcularTotalXP());
+            System.out.println("XP dev2:"+ dev2.calcularTotalXP());
+        }
 
     }
 }
--- src/br/com/lucasmadeira/dominio/Curso.java
+++ src/br/com/lucasmadeira/dominio/Curso.java
@@ -1,25 +1,8 @@
 package br.com.lucasmadeira.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends  Conteudo {
+
     private int cargaHoraria;
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public int getCargaHoraria() {
         return cargaHoraria;
@@ -30,11 +13,14 @@ public class Curso {
     }
 
     @Override
+    public double calcularXP() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
-                ", cargaHoraria=" + cargaHoraria +
-                '}';
+                "cargaHoraria=" + cargaHoraria +
+                "} " + super.toString();
     }
 }
